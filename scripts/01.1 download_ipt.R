bea85bf390937157b411a02f10032a5d2ece6609
josewribeiro <josew@cncflora.net>
2019-10-29 14:45:09 -0300

    Deletei script que estava na pasta errada.

--- scripts/01.1 download_ipt.R
+++ scripts/01.1 download_ipt.R
@@ -3,11 +3,6 @@
    ###              e formatar a distribuição e forma de vida das plantas                     ###               
 ####################################################################################################
 
-
-#########################################################
-### Baixar dados do Projeto Flora do Brasil 2020 (IPT) 
-### 
-
 # Ler pacotes
 library(readr)
 library(dplyr)
