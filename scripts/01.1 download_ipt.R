da6d52668a827535ed02e37599bd749f8800a139
josewribeiro <josew@cncflora.net>
2019-10-29 14:29:47 -0300

    Adicionar cabeçalho no script.

--- scripts/01.1 download_ipt.R
+++ scripts/01.1 download_ipt.R
@@ -1,4 +1,12 @@
-### Baixar dados do Projeto Flora do Brasil 2020 e formatar a distribuição e forma de vida das plantas
+####################################################################################################
+   ###                Baixar dados do Projeto Flora do Brasil 2020 (IPT)                      ###
+   ###              e formatar a distribuição e forma de vida das plantas                     ###               
+####################################################################################################
+
+
+#########################################################
+### Baixar dados do Projeto Flora do Brasil 2020 (IPT) 
+### 
 
 # Ler pacotes
 library(readr)
