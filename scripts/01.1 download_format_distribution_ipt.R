5ed37b4f9a2cc4ca1a0f7927e4b634f38b9f6af9
josewribeiro <josew@cncflora.net>
2019-10-29 14:25:02 -0300

    Colocar todos os scripts dentro de uma subpasta chamada scripts.

---
+++ scripts/01.1 download_format_distribution_ipt.R
@@ -0,0 +1,9 @@
+### Baixar dados do Projeto Flora do Brasil 2020 e formatar a distribuição e forma de vida das plantas
+
+# Ler pacotes
+library(readr)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(jsonlite)
+library(downloader)
