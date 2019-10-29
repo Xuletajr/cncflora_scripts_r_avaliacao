06f055edc926faa4a383f552162d46f80abd44e4
josewribeiro <josew@cncflora.net>
2019-10-29 15:22:53 -0300

    Código para baixar os dados do Flora do Projeto Flora do Brasil 2020 (IPT). Tem que ajustar o argumento "mode" da funçao "download" dependendo do sistema operacional do computador. Para windows funcionou mode = "wb".

--- scripts/01.1 download_ipt.R
+++ scripts/01.1 download_ipt.R
@@ -10,3 +10,9 @@ library(tidyr)
 library(stringr)
 library(jsonlite)
 library(downloader)
+
+# Baixar dados do Flora do Projeto Flora do Brasil 2020 (IPT)---
+pag <- "http://ipt.jbrj.gov.br/jbrj/archive.do?r=lista_especies_flora_brasil"
+download(url = pag, destfile = "iptflora", mode = "wb") # Para sistema operacional windows só funciona com mode = "wb"
+unzip("iptflora", exdir = "./ipt") # salvar em pasta "ipt" dentro da pasta de trabalho
+
