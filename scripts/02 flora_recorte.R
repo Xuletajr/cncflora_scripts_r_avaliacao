f1d20b18ad6bef5242c99f70aeb9a9a82a4b2e76
josewribeiro <josew@cncflora.net>
2019-11-13 15:21:59 -0300

    pequena formatação

--- scripts/02 flora_recorte.R
+++ scripts/02 flora_recorte.R
@@ -15,10 +15,31 @@ flora <- read_csv("./ipt/all_flora.csv", locale = locale(encoding = "UTF-8")) %>
 head(flora)
 
 # Ler os dados com as espécies que será trabalhadas no recorte----
-# A planilha precisa ter ao menos uma coluna denominada "scientificName" que contenha o nome científico + autoria das espécies
+# A planilha precisa ter ao menos uma coluna denominada "scientificName" que contenha o nome científico + autoria 
+# e uma coluna com as famílias das espécies
 treespp <- read_csv("./results/plantas_selecionadas_2019_bloco3.csv") %>%
    dplyr::select(-1) %>%
-   # Função do pacote "flora" que retira autoria do nome das espécies. Para manter certa padronização seria importante
-   # sempre que entrar uma planilha a coluna com o binômio das espécies seja gerada com essa função. 
+   # Função do pacote "flora" que retira autoria do nome científico das espécies. Para manter padronização seria importante
+   # sempre que entrar uma planilha a coluna com o binômio das espécies ser gerada com essa função. 
    mutate(nome_especie = purrr::map(scientificName, ~remove.authors(.)) %>%
-             simplify2array()) 
+             simplify2array())
+
+treespp2 <- treespp %>%
+   dplyr::select(Familia, scientificName, nome_especie) %>%
+   rename(original_name = scientificName) %>%
+   left_join(flora) %>%
+   dplyr::select(
+      Familia,
+      original_name,
+      scientificName,
+      nome_especie,
+      taxonID,
+      acceptedNameUsageID,
+      acceptedNameUsage,
+      parentNameUsage,
+      family,
+      taxonomicStatus,
+      nomenclaturalStatus,
+      taxonRank,
+      vernacular_names,
+      nome_especie)
