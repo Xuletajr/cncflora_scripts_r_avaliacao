17277b9347c297b12626beb6296d89262a0acbd4
josewribeiro <josew@cncflora.net>
2019-11-26 14:02:59 -0300

    Iniciando o script para baixar dados do speciesLink.

---
+++ scripts/03.2 download occs speciesLink.R
@@ -0,0 +1,11 @@
+##############################################################################################################
+###                               Baixar e organizar dados do speciesLink                            ###        
+##############################################################################################################
+
+# Ler os pacotes
+library(readxl)
+library(dplyr)
+library(tidyr)
+library(biogeo)
+library(readr)
+library(stringr)
