18cafb9574d64822d65ff40ab815e6fdbfb85fd2
Xuletajr <jwrj@rc.unesp.br>
2020-01-10 17:12:45 -0300

    Atualizando código.

--- scripts/03.3 occs REFLORA.R
+++ scripts/03.3 occs REFLORA.R
@@ -1,3 +1,20 @@
 ##############################################################################################################
    ###                                Baixar e organizar dados do REFLORA                               ###        
 ##############################################################################################################
+
+# Ler pacotes
+library(readxl)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(lubridate)
+library(biogeo)
+library(readr)
+library(flora)
+
+# Ler a planilha com as espécies de plantas do recorte
+treespp <- readr::read_csv("./results/names_flora.csv")
+
+# Colocar as famílias e binômio das espécies em vetores.  
+familias <- treespp$final_family
+especies <- treespp$nome_especie
