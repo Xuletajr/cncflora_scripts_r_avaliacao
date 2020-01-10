91b1c13e773e30bf2e16afd6a8f1c6d0c16d4973
Xuletajr <jwrj@rc.unesp.br>
2020-01-10 17:21:33 -0300

    Exportando todas as ocorrências em uma única planilha.

--- scripts/03.3 occs REFLORA.R
+++ scripts/03.3 occs REFLORA.R
@@ -31,6 +31,16 @@ reflora_7 <- readxl::read_excel("./data/RelatorioConsultaTestemunho_grupo7.xlsx"
 reflora_8 <- readxl::read_excel("./data/RelatorioConsultaTestemunho_grupo8.xlsx", sheet = 1)
 reflora_9 <- readxl::read_excel("./data/RelatorioConsultaTestemunho_grupo9.xlsx", sheet = 1)
 
-# Juntando as planilhas em uma
+# Juntando as planilhas
 reflora_all <- dplyr::bind_rows(reflora_1, reflora_2, reflora_3, reflora_4,
                          reflora_5, reflora_6, reflora_7, reflora_8, reflora_9)
+
+# Exportar uma planilha csv com todos os dados do reflora juntos
+write.csv(reflora_all, "./results/reflora_all.csv", fileEncoding = "UTF-8")
+
+#
+reflora_all <- read.csv("./results/reflora_all.csv", fileEncoding = "UTF-8") %>% 
+   select(-1) %>% 
+   dplyr::mutate(G?nero = str_to_title(G?nero)) %>%
+   dplyr::mutate(Esp?cie = str_to_lower(Esp?cie)) %>%
+   tidyr::unite("nome_especie", G?nero, Esp?cie, remove = FALSE, sep = " ") 
