d5d6b294315bb8524a8025930c5aba8249fcebdb
Xuletajr <jwrj@rc.unesp.br>
2020-01-10 16:11:12 -0300

    Lendo a planilha de ocorrências do speciesLink.

--- scripts/03.2 occs speciesLink.R
+++ scripts/03.2 occs speciesLink.R
@@ -21,3 +21,20 @@ treespp <- readr::read_csv("./results/names_flora.csv")
 # Colocar as famílias e binômio das espécies em vetores.  
 familias <- treespp$final_family
 especies <- treespp$nome_especie
+
+# Ler a tabela com todas as ocorrências provenientes do speciesLink
+# Estas ocorrências foram baixadas em planilhas separadas e juntadas em uma planilha só previamente
+speciesLink <- read_csv("./results/speciesLink_geral.csv",
+                        locale = locale(encoding = "UTF-8"))
+
+# Filtra somente os nomes das espécies iguais aos da nossa lista (alguns nomes vêm desconfigurados)
+speciesLink2 <- speciesLink %>% filter(scientificname %in% especies) %>%
+   mutate(nome_especie = scientificname) %>% rename(notas  = notes) %>% 
+   left_join(treespp) %>%
+   dplyr::mutate(acceptedNameUsage = scientificName) %>%
+   dplyr::mutate(scientificNameAuthorship = word(acceptedNameUsage, 3, -1, sep = " "))
+
+# Conferindo algumas informações da planilha
+names(speciesLink2) # variáveis disponíveis na planilha
+speciesLink2$scientificNameAuthorship # Se os nomes vieram escritos corretamente, dependendo do "enconding" pode desconfigurar
+speciesLink2 %>% count(is.na(scientificNameAuthorship)) # Quantos nomes vieram sem autor (i.e., NA's)
