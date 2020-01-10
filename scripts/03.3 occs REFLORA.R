bc3eba1d1ee26325e32610f9abe1d9649c40f0c4
Xuletajr <jwrj@rc.unesp.br>
2020-01-10 17:17:31 -0300

    Juntando as planilha.

--- scripts/03.3 occs REFLORA.R
+++ scripts/03.3 occs REFLORA.R
@@ -21,12 +21,16 @@ especies <- treespp$nome_especie
 
 # Ler as planilhas com dados reflora - Os dados foram baixados com script do Vicente no NODE (disponível no Bitbucket dele)
 # Próximo passa seria automatizar o script para baixar diretamente as ocorrências do REFLORA
-reflora_1 <- read_excel("./data/RelatorioConsultaTestemunho_grupo1.xlsx", sheet = 1)
-reflora_2 <- read_excel("./data/RelatorioConsultaTestemunho_grupo2.xlsx", sheet = 1)
-reflora_3 <- read_excel("./data/RelatorioConsultaTestemunho_grupo3.xlsx", sheet = 1)
-reflora_4 <- read_excel("./data/RelatorioConsultaTestemunho_grupo4.xlsx", sheet = 1)
-reflora_5 <- read_excel("./data/RelatorioConsultaTestemunho_grupo5.xlsx", sheet = 1)
-reflora_6 <- read_excel("./data/RelatorioConsultaTestemunho_grupo6.xlsx", sheet = 1)
-reflora_7 <- read_excel("./data/RelatorioConsultaTestemunho_grupo7.xlsx", sheet = 1)
-reflora_8 <- read_excel("./data/RelatorioConsultaTestemunho_grupo8.xlsx", sheet = 1)
-reflora_9 <- read_excel("./data/RelatorioConsultaTestemunho_grupo9.xlsx", sheet = 1)
+reflora_1 <- readxl::read_excel("./data/RelatorioConsultaTestemunho_grupo1.xlsx", sheet = 1)
+reflora_2 <- readxl::read_excel("./data/RelatorioConsultaTestemunho_grupo2.xlsx", sheet = 1)
+reflora_3 <- readxl::read_excel("./data/RelatorioConsultaTestemunho_grupo3.xlsx", sheet = 1)
+reflora_4 <- readxl::read_excel("./data/RelatorioConsultaTestemunho_grupo4.xlsx", sheet = 1)
+reflora_5 <- readxl::read_excel("./data/RelatorioConsultaTestemunho_grupo5.xlsx", sheet = 1)
+reflora_6 <- readxl::read_excel("./data/RelatorioConsultaTestemunho_grupo6.xlsx", sheet = 1)
+reflora_7 <- readxl::read_excel("./data/RelatorioConsultaTestemunho_grupo7.xlsx", sheet = 1)
+reflora_8 <- readxl::read_excel("./data/RelatorioConsultaTestemunho_grupo8.xlsx", sheet = 1)
+reflora_9 <- readxl::read_excel("./data/RelatorioConsultaTestemunho_grupo9.xlsx", sheet = 1)
+
+# Juntando as planilhas em uma
+reflora_all <- dplyr::bind_rows(reflora_1, reflora_2, reflora_3, reflora_4,
+                         reflora_5, reflora_6, reflora_7, reflora_8, reflora_9)
