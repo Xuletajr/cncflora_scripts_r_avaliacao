83083c269d857e249c0de0949b29b5289bbf75b4
Xuletajr <jwrj@rc.unesp.br>
2020-01-11 18:17:48 -0300

    Exportando as planilhas com nome de coletor do INPA corrigidos.

--- scripts/03.4 juntar occs.R
+++ scripts/03.4 juntar occs.R
@@ -12,6 +12,13 @@ library(plyr)
 # Função change_NA_to_df.R disponível em: https://stevenmortimer.com/the-unfinished-duplicated-function/
 source("./functions/new_duplicated.R") 
 
+# Ler a planilha com as espécies de plantas do recorte
+treespp <- readr::read_csv("./results/names_flora.csv")
+
+# Colocar as famílias e binômio das espécies em vetores.  
+familias <- treespp$final_family
+especies <- treespp$nome_especie
+
 ###
 # Ajustar a coluna de coletor do inpa provenientes do GBIF (só vem o nome no curador como coletor)
 # Ler planilha contendo apnenas ocorrências do INPA compilada com dados do speciesLink
@@ -22,43 +29,48 @@ tabela_inpa_splink <- read.csv(file = "./results/inpa_filtrado.csv", header = TR
 unique(tabela_inpa_splink$institutioncode)
 
 tabela_inpa_splink <- tabela_inpa_splink %>%
-   mutate(institutioncode = "Instituto Nacional de Pesquisas da Amazônia (INPA)")
+   dplyr::mutate(institutioncode = "Instituto Nacional de Pesquisas da Amazônia (INPA)")
 
-#todos os coletores do inpa
+# Todos os coletores do inpa
 colectores_inpa <- tabela_inpa_splink %>% 
    dplyr::select(institutioncode, catalognumber, collector) %>% 
-   mutate(collector = as.character(collector), catalognumber = as.character(catalognumber)) %>% 
-   rename(institutionCode = institutioncode, catalogNumber = catalognumber)#catalogNumber = catalognumber, 
+   dplyr::mutate(collector = as.character(collector), catalognumber = as.character(catalognumber)) %>% 
+   dplyr::rename(institutionCode = institutioncode, catalogNumber = catalognumber)#catalogNumber = catalognumber, 
 
-#loop----
+# Loop----
 for (i in 1:length(especies)) {
-   #nomes
+   # Nomes
    nome_arquivo <- paste0("./output_final/", familias[i],"/",familias[i],"_", especies[i],"_", "gbif raw.csv")
    nome_out <- paste0("./output_final/", familias[i],"/",familias[i],"_", especies[i],"_", "inpa.csv")
    print(paste("Inpa", especies[i], i, "de", length(especies), sep = " "))
    if(!file.exists(nome_out)) {
-      #le a tabela
-      tabela_especie <- read.csv(nome_arquivo, stringsAsFactors = F, encoding = "UTF-8") %>% # row.names = 1,
+      # Lê a tabela
+      tabela_especie <- read.csv(nome_arquivo, stringsAsFactors = F, encoding = "UTF-8") %>% 
          mutate(catalogNumber = factor(catalogNumber))
       
-      #junta a tabela com os coletores
+      # Junta a tabela com os coletores
       if ("Instituto Nacional de Pesquisas da Amaz?nia (INPA)" %in% tabela_especie$institutionCode) {
          proof_name <- paste0("./output_final/", familias[i],"/",familias[i],"_", especies[i],"_", "inpa_proof.csv")
          tabela_especie <- left_join(tabela_especie, colectores_inpa)
-         #tudo o que for do inpa bota o coletor e tira a coluna collector
+         # Tudo o que for do inpa bota o coletor e tira a coluna collector
          tabela_especie <- tabela_especie %>%
             mutate(recordedBy = if_else(tabela_especie$institutionCode %in%
                                            "Instituto Nacional de Pesquisas da Amaz?nia (INPA)", collector, recordedBy)) %>%
             dplyr::select(-collector)
-         #escreve
+         
+         # Exportar as planilhas
          write.csv(tabela_especie, file = nome_out, row.names = F, fileEncoding = "UTF-8", na = "")
          proof <- tabela_especie %>%
             filter(institutionCode == "Instituto Nacional de Pesquisas da Amaz?nia (INPA)") %>%
             dplyr::select(institutionCode, catalogNumber, recordedBy) %>%
             distinct()
          write.csv(proof, file = proof_name, row.names = F, fileEncoding = "UTF-8",  na = "")
-      } else {
+      
+         } else {
+         
          write.csv(tabela_especie, file = nome_out, row.names = F, fileEncoding = "UTF-8", na = "")
       }
    }
 }
+
+#
