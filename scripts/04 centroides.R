a978390ccab28622d2223b91f2c1d9273784ba13
Xuletajr <jwrj@rc.unesp.br>
2020-01-11 20:09:01 -0300

    Ler pacotes necessários para o script.

--- scripts/04 centroides.R
+++ scripts/04 centroides.R
@@ -1,3 +1,14 @@
 ##############################################################################################################
    ###        Limpezas espaciais e adicionar centróides dos municípios nas coordenadas                  ###        
 ##############################################################################################################
+
+# Ler pacotes
+library(dplyr)
+library(stringr)
+library(rgbif)
+library(readxl)
+library(textclean)
+library(flora)
+library(lubridate)
+# Função "filt_andrea.R" disponível em:
+source("./functions/filt_andrea.R")
