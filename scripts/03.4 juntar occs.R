d3035714d138f8b4b25bcfd4bab04e9ac627c095
Xuletajr <jwrj@rc.unesp.br>
2020-01-11 19:56:51 -0300

    Fazendo limpezas nas ocorrências.

--- scripts/03.4 juntar occs.R
+++ scripts/03.4 juntar occs.R
@@ -74,7 +74,7 @@ for (i in 1:length(especies)) {
 }
 
 ###
-# Cria um diretório para juntar as três fontes de dados (GBIF, REFLORA E speciesLINK)
+# Criar um diretório para juntar as três fontes de dados (GBIF, REFLORA E speciesLINK)
 dir.create("output_final2")
 
 # Loop para juntar as três fontes de dados
@@ -108,5 +108,90 @@ for (i in 1:length(especies)) {
    }
 }
 
+### Limpeza de registros com função "new_duplicated.R"
+# Criar um diretório para colocar as ocorrências com limpeza de registros
+dir.create("output_final3")
+
+for (i in 1:length(especies)) {
+   print(paste("Limpando", especies[i], i, "de", length(especies), sep = " "))
+   
+   dir.create(paste0("./output_final3/",familias[i]), showWarnings = F)
+   
+   ### O arquivo com o inpa
+   nome_juntas <- paste0("./output_final2/", familias[i],"/",familias[i],"_", especies[i],"_", "juntas.csv")
+   
+   ### O nome do arquivo que será criado com registros excluídos
+   nome_excluded <- paste0("./output_final3/",familias[i],"/",familias[i], "_", especies[i],"_",
+                           "excluded.csv")
+   
+   ### O nome do arquivo que será criado com registros duplicados (coletor, ano, numero de coleta). nao incluo nos excluded porque ele não vai ser excluido, vai ter uma cópia dele em clean e o duplicado não pode aparecer no excluded, vai ser confuso. mas eu quero checar que os s.n. não foram tomados como duplicados:
+   nome_duplicata <- paste0("./output_final3/",familias[i],"/",familias[i], "_", especies[i],"_",
+                            "duplicata.csv")
+   
+   ### O nome do arquivo limpo que será criado no final
+   nome_clean <- paste0("./output_final3/",familias[i],"/",familias[i], "_", especies[i],"_",
+                        "clean.csv")
+   
+   ### Agora a "tabela" é tabela_especie
+   tabela_especie <- read_csv(nome_juntas, locale = locale(encoding = "UTF-8"),
+                              na = c("", "NA")) # 
+   
+   # Remover registros não informativos, sem coletor, numero de coleta, ano e informações de localidade----
+   # Seleciona os registros que devem sair
+   tabela_exclude1 <- tabela_especie %>% dplyr::filter(is.na(municipality) & is.na(locality))
+   
+   # A tabela de espécies menos esses registros (antijoin)
+   tabela_especie2 <- dplyr::anti_join(tabela_especie, tabela_exclude1)
+   
+   # Remover registros fora do Brasil ou que não tem informação de país----
+   # seleciona os registros que devem sair
+   tabela_exclude2 <- tabela_especie2 %>% dplyr::filter(is.na(country) | !country %in% 
+                                                           c("Brazil", "Brasil","BRAZIL", "BRASIL", "Brésil", "brésil"))
+  
+   # A tabela de especie menos esses registros (antijoin)
+   tabela_especie3 <- dplyr::anti_join(tabela_especie2, tabela_exclude2)
+   
+   # Registros que não tem nome de coletor nem numero de coleta
+   # Seleciona os registros que devem sair
+   tabela_exclude3 <- tabela_especie3 %>%
+      dplyr::filter(is.na(recordedBy) & is.na(recordNumber))
+   
+   # A tabela de especie menos esses registros (antijoin)
+   tabela_especie4 <- dplyr::anti_join(tabela_especie3, tabela_exclude3)
+   
+   # Juntar todas as tabelas do que seria exclu????do (cada passo gerou tabelas diferentes)
+   tabela_exclude_final <- rbind.fill(tabela_exclude1, tabela_exclude2, tabela_exclude3)#dplyr::bind_rows(tabela_exclude1, tabela_exclude2, tabela_exclude3)
+   write.csv(tabela_exclude_final, file = nome_excluded, fileEncoding = "UTF-8", 
+             na = "", row.names = FALSE)
+   
+   # Remover registros com número de coleta, estado e ano iguais (coletas colaborativas duplicadas)
+   # gera um vetor TrueFalse dizendo quem é duplicado, omitindo os s.n e NA
+   vetor_duplicata <- tabela_especie4 %>%
+      dplyr::select(year, recordNumber, stateProvince) %>%
+      new_duplicated(., incomparables = c("NA", "s.n", "s/n"))
+   
+   # Cria a tabela de duplicados e salva
+   tabela_duplicata <- tabela_especie4[vetor_duplicata,]
+   
+   # Tirar os duplicados da tabela especie
+   tabela_especie4 <- tabela_especie4[!vetor_duplicata,]
+   
+   ####################################################
+   # Registros com mesmo coletor e número de coleta
+   # Precisa incluir escape para s.n. e NA {next}
+   vetor_duplicata <- tabela_especie4 %>% dplyr::select(recordedBy, recordNumber) %>%
+      new_duplicated(., incomparables = c("s.n", "s/n"))
+   
+   # Criar a tabela de duplicados e salvar
+   tabela_duplicata <- bind_rows(tabela_duplicata, tabela_especie4[vetor_duplicata,])
+   
+   write.csv(tabela_duplicata, file = nome_duplicata, fileEncoding = "UTF-8", 
+             na = "", row.names = FALSE)
+   
+   # Tirar os duplicados da tabela especie
+   tabela_especie4 <- tabela_especie4[!vetor_duplicata,]
+   write.csv(tabela_especie4, file = nome_clean, fileEncoding = "UTF-8", na = "", row.names=FALSE)
+   
+}
 
 ######   end----
