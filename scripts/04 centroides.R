baeac7d134c2e6bc6d9d872c0d7f6fcddf86358e
Xuletajr <jwrj@rc.unesp.br>
2020-01-12 17:30:32 -0300

    Checando quais municípios possuem mesmo nome  que Estados.

--- scripts/04 centroides.R
+++ scripts/04 centroides.R
@@ -43,3 +43,29 @@ tabela_centroides_ucs$uc
 # Ler shape dos estados
 estados <- rgdal::readOGR(dsn = "./data/shape/Limites_v2017", layer = "lim_unidade_federacao_a",
                           encoding = "UTF-8", use_iconv = TRUE)
+
+estados$nome
+
+# Tabela com a sigla pois uma limpeza é substituir a sigla ("rj") pelo nome completo
+sigla_estados <- estados@data[,c("nome", "sigla")] %>%
+   data.frame() %>%
+   mutate(stateProvince = replace_non_ascii(tolower(nome))) %>%
+   mutate(sigla = replace_non_ascii(tolower(sigla)))
+
+
+# Centroides estados---- já deixa em minusculo sem acento tudo
+centroides_estados <- rgeos::gCentroid(estados, byid = T, id = estados$nome) %>%
+   data.frame %>%
+   tibble::rownames_to_column(var = "estados_shp") %>%
+   mutate(stateProvince = replace_non_ascii(tolower(estados_shp))) %>%
+   rename(x_estado = x, y_estado = y) %>%
+   left_join(sigla_estados)
+
+centroides_estados
+
+# Comparar o nome dos estados e dos municipios porque há municipios com o mesmo nome de alguns estados, importante para a limpeza----
+sort(setdiff(centroides_estados$stateProvince, tabela_centroides$municipality))
+
+# Mesmo nome estado e municipio
+tabela_centroides[which(tabela_centroides$municipality %in%
+                           centroides_estados$stateProvince),]
