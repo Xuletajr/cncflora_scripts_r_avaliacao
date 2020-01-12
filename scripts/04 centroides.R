d78356fecd7429ab3845afdb949cc38f33eeada2
Xuletajr <jwrj@rc.unesp.br>
2020-01-12 16:17:41 -0300

    Ler a planilha com a lista de coordenadas por centroide.

--- scripts/04 centroides.R
+++ scripts/04 centroides.R
@@ -10,5 +10,16 @@ library(readxl)
 library(textclean)
 library(flora)
 library(lubridate)
-# Função "filt_andrea.R" disponível em:
+# Função "filt_andrea.R"
 source("./functions/filt_andrea.R")
+
+# Ler a planilha com as espécies de plantas do recorte
+treespp <- readr::read_csv("./results/names_flora.csv")
+familias <- treespp$final_family
+especies <- treespp$nome_especie
+
+# Ler a planilha com a lista de coordenadas por centroide
+tabela_centroides <- read.delim(file = "./data/centroide_municipio.csv",
+                                header = TRUE, sep = ";",
+                                stringsAsFactors = FALSE,
+                                fileEncoding = "ISO-8859-9")
