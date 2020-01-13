bd3223fb6c1375d52bfc98650aabb3d819986c53
Xuletajr <jwrj@rc.unesp.br>
2020-01-13 12:05:11 -0300

    Atualizando o script com alguns comentários.

--- scripts/05 AOO_EOO.R
+++ scripts/05 AOO_EOO.R
@@ -27,26 +27,29 @@ especies <- treespp$nome_especie
 familias <- treespp$final_family
 
 # Criar uma pasta para colocar as planilhas com estimativas de EOO e AOO
-dir.create("./_aaoeoo/")
+dir.create("./aaoeoo/")
 
-# Loop para estimar EOO e AOO para cada espécie.
+# Loop para estimar EOO e AOO para cada espécie
+# Para o loop funcionar o mapa do Brasil precisa estar plotada e aberto no painel de gráfico
+# do Rstudio, pois há um comando para plotar os pontos de ocorrência no mapa. Se não tiver com mapa aberto dará um erro:
+# "Error in plot.xy(xy.coords(x, y), type = type, ...) : invalid graphics state"
 # Conforme tem acontecido com os loops anteriores, este também tem para em algumas circustâncias
 for (i in 1:length(especies)) { 
    print(paste(especies[i], familias[i], "- calculating AOO and EOO", i, "of", length(especies)))
    nome_final <- paste0("./output_final5/",familias[i],"/",familias[i], "_",
                         especies[i],"_", "final.csv")
    
-   data_csv1 <- paste0("./_aaoeoo/", familias[i], "_", especies[i],"_", "aoo_eoo.csv")
+   data_csv1 <- paste0("./aaoeoo/", familias[i], "_", especies[i],"_", "aoo_eoo.csv")
    
    tabela.final <- read.csv(nome_final) 
    
    tabela <- tabela.final[complete.cases(tabela.final[,c("decimalLongitude", "decimalLatitude")]),]
    coord <- tabela[,c("decimalLongitude", "decimalLatitude")]
-   pts <- SpatialPoints(coord)
-   proj4string(pts) <- CRS("+proj=longlat +datum=WGS84")
-   pts.utm <- spTransform(pts, CRS("+init=epsg:29193"))
+   pts <- sp::SpatialPoints(coord)
+   proj4string(pts) <- sp::CRS("+proj=longlat +datum=WGS84")
+   pts.utm <- sp::spTransform(pts, CRS("+init=epsg:29193"))
    
-   aoo2 <- getAOO(pts.utm, 2000)
+   aoo2 <- redlistr::getAOO(pts.utm, 2000)
    red_aoo2 <- round(red::aoo(pts.utm@coords), digits = 2)
    
    # Só estimar EOO se a planilha de ocorrências tiver mais de 3 ocorrências
@@ -62,12 +65,12 @@ for (i in 1:length(especies)) {
       warning(paste(especies[i], "has less than 4 coordinates", "\n"))
       
    }  else { 
-      eoo <- makeEOO(pts.utm)
-      eoo_area <- getAreaEOO(eoo)
+      
+      eoo <- redlistr::makeEOO(pts.utm)
+      eoo_area <- redlistr::getAreaEOO(eoo)
       eoo_conr <- ConR::EOO.computing(coord, Name_Sp = especies[i], write_shp = T)
       points(coord)
-      aoo10 <- getAOO(pts.utm, 10000)
-      
+      aoo10 <- redlistr::getAOO(pts.utm, 10000)
       
       data <- data.frame(nome_especie = especies[i],
                          final_family = familias[i],
