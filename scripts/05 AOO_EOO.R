5304ab4ab936347dc38b620fbc58267a2158e55b
Xuletajr <jwrj@rc.unesp.br>
2020-01-13 11:41:10 -0300

    Ler pacotes, plotando mapa Brasil e ler planilha de espécies.

--- scripts/05 AOO_EOO.R
+++ scripts/05 AOO_EOO.R
@@ -1,3 +1,25 @@
 ##############################################################################################################
    ###                                        Estimando EOO e AOO                                       ###        
 ##############################################################################################################
+
+# Ler pacotes
+library(readxl)
+library(textclean)
+library(dplyr)
+library(stringr)
+library(flora)
+library(red)
+library(ConR)
+library(rgdal)
+
+# Plotando mapa do Brasil e Américas.
+a <- maps::map(,c("Brazil","Mexico"))
+a$range
+b <- maps::map(add = T)
+b$range
+
+# Ler a planilha com as espécies de plantas do recorte
+treespp <- readr::read_csv("./results/names_flora.csv")
+
+especies <- treespp$nome_especie
+familias <- treespp$final_family
