715bd431b9282171a4abbe07fd4c50062e4c972d
Xuletajr <jwrj@rc.unesp.br>
2020-01-14 14:58:56 -0300

    Juntando as informações de ocorrências nas UCs.

--- scripts/09 UCs.R
+++ scripts/09 UCs.R
@@ -6,6 +6,8 @@
 library(rgdal)
 library(dplyr)
 library(sf)
+library(readr)
+library(purrr)
 
 # Ler shape com as Unidades de Conservação
 uc <- rgdal::readOGR(dsn = "./data/shape/UCs_WGS84", layer = "UCs_wgs84", 
@@ -15,9 +17,14 @@ st_crs(uc)
 proj4string(uc)
 uc$NOME_UC1
 
+# Projetando as UCs
+proj4string(uc) <-  CRS("+proj=longlat +datum=WGS84")
+uc <- spTransform(uc, CRS=CRS("+proj=longlat +datum=WGS84"))
+st_crs(uc)
+class(uc)
+
 # Ler a planilha com as espécies de plantas do recorte
-treespp <- read.csv("./results/aoo_veg_cites.csv", row.names = 1, 
-                    fileEncoding = "UTF-8")
+treespp <- readr::read_csv("./results/aoo_veg_cites.csv")
 
 especies <- treespp$nome_especie
 familias <- treespp$final_family
@@ -40,6 +47,7 @@ for (i in 1:length(especies)) {
    
    nome_final <- paste0("./output_final5/",familias[i],"/",familias[i], "_",
                         especies[i],"_", "final.csv")
+   
    dir.create(paste0("./UC/",familias[i]), showWarnings = F)
    uc_output <- paste0("./UC/", familias[i], "/", familias[i], "_", especies[i],"_", "UC.csv")
    
@@ -59,13 +67,13 @@ for (i in 1:length(especies)) {
    n_in_uc <- over_results %>% 
       dplyr::filter(!is.na(ID_UC0)) %>% 
       dplyr::count() %>% 
-      dplyr::pull
+      pull
    
    n_uc <- over_results %>% 
       dplyr::filter(!is.na(ID_UC0)) %>% 
-      distinct::distinct(NOME_UC1) %>% 
+      dplyr::distinct(NOME_UC1) %>% 
       dplyr::count() %>% 
-      dplyr::pull
+      pull
    
    which_uc <- ifelse(n_in_uc == 0, NA, over_results %>%
                          dplyr::filter(!is.na(ID_UC0)) %>%
@@ -82,4 +90,16 @@ for (i in 1:length(especies)) {
    write.csv(final_res, file = uc_output, fileEncoding = "UTF-8")
    
 }
+
+# Juntando as informações de ocorrências nas UCs
+tabela_UCs <- list.files("./UC", full.names = T, pattern = "UC.csv$", recursive = T) %>%
+   purrr::map(.f = readr::read_csv) %>%
+   dplyr::bind_rows() %>%
+   dplyr::select(-1)  %>%  group_by(nome_especie) %>% 
+   dplyr::mutate(UCs = paste(UC, collapse = ", ")) %>%
+   dplyr::select(-UC) %>% distinct() %>% ungroup()
+
+tabela_UCs
+
+write.csv(tabela_UCs, "./results/tabela_UCs.csv", na = "", fileEncoding = "UTF-8")
 
