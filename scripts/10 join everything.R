bc73fc2b574e996c9f443875fbd3bd91c0dcdde1
Xuletajr <jwrj@rc.unesp.br>
2020-01-14 15:48:46 -0300

    Pequenos ajuses no script de juntar tudo.

---
+++ scripts/10 join everything.R
@@ -0,0 +1,37 @@
+##############################################################################################################
+   ###                                      Juntando todos os dados                                     ###        
+##############################################################################################################
+
+# Ler pacotes
+library(tidyverse)
+library(readr)
+
+# Ler as diferentes planilhas
+# Tava dando errado usando read.csv
+#aoo <- read.csv("./results/aoo_veg_cites.csv", row.names = 1, fileEncoding = "UTF-8")
+
+aoo <- readr::read_csv("./results/aoo_veg_cites.csv")
+use <- read.csv("./results/Use_results_general.csv", row.names = 1,  fileEncoding = "UTF-8") %>%
+   rename(nome_especie = especies)
+use <- readr::read_csv("./results/Use_results_general.csv")
+ucs <- read.csv("./results/tabela_UCs.csv", row.names = 1, fileEncoding = "UTF-8") %>% 
+   dplyr::rename(nusado_ucs = nusado)
+coleta <- read.csv("./results/spp_coleta.csv", row.names = 1, fileEncoding = "UTF-8")
+
+names(aoo)
+names(use)
+names(ucs)
+names(coleta)
+
+# Juntar tudo
+final <- left_join(aoo, use) %>% left_join(ucs) %>%
+   dplyr::left_join(coleta) %>%
+   dplyr::arrange(final_family, nome_especie) %>% 
+   dplyr::mutate(scientificNameAuthorship = word(scientificName, 3, -1, sep = " "))
+
+View(final)
+
+# Exportar planilha com as informações juntas
+write.csv(final, "./results/final_results.csv", fileEncoding = "UTF-8")
+
+######   end----
